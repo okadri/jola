fc5a2da9723f77889f4317f6935da06c78ca54b0
Obada Kadri <brobada@gmail.com>
2021-09-26 00:13:37 -0700

    Successfully pulls and displays contact data

--- src/initSupabase.ts
+++ src/initSupabase.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import {REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY} from "@env";
+import { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY } from "@env";
 
 const supabaseUrl = REACT_APP_SUPABASE_URL
 const supabaseKey = REACT_APP_SUPABASE_ANON_KEY
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { View, Linking } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { MainStackParamList } from "../types/navigation";
 import { StackScreenProps } from "@react-navigation/stack";
 import { supabase } from "../initSupabase";
 import {
   Layout,
   Button,
-  Text,
   TopNav,
   Section,
   SectionContent,
@@ -14,27 +13,82 @@ import {
   themeColor,
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
+/** URL polyfill. Required for Supabase queries to work in React Native. */
+import 'react-native-url-polyfill/auto'
+
+type Contact = {
+  id: string
+  name: string
+  email: string
+}
 
 export default function ({
   navigation,
 }: StackScreenProps<MainStackParamList, "MainTabs">) {
   const { isDarkmode, setTheme } = useTheme();
+  const [loading, setLoading] = useState(true)
+  const [contacts, setContacts] = useState<Array<Contact>>([])
+
+    const renderItem = ({item}: {item: Contact}) => {
+      return (
+        <View style={styles.contactCard}>
+          <Text style={styles.contactName}>{item.name}</Text>
+        </View>
+      );
+    }
+  
+  useEffect(() => {
+    getContacts()
+  }, [])
+
+  async function getContacts() {
+    try {
+      setLoading(true)
+      const user = supabase.auth.user()
+      const { data: contacts, error } = await supabase
+        .from<Contact>('contacts')
+        .select('*')
+
+      if (error) console.log('error', error)
+      else setContacts(contacts!)
+    } catch (error) {
+      console.log('error', error)
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
     <Layout>
       <TopNav
         middleContent="Home"
-        rightContent={
+        leftContent={
           <Ionicons
             name={isDarkmode ? "sunny" : "moon"}
             size={20}
             color={isDarkmode ? themeColor.white100 : themeColor.dark}
           />
         }
-        rightAction={() => {
+        leftAction={() => {
           if (isDarkmode) {
             setTheme("light");
           } else {
             setTheme("dark");
+          }
+        }}
+        rightContent={
+          <Ionicons
+            name="log-out"
+            size={20}
+            color={isDarkmode ? themeColor.white100 : themeColor.dark}
+          />
+        }
+        rightAction={async () => {
+          const { error } = await supabase.auth.signOut();
+          if (!error) {
+            alert("Signed out!");
+          }
+          if (error) {
+            alert(error.message);
           }
         }}
       />
@@ -45,37 +99,18 @@ export default function ({
           justifyContent: "center",
         }}
       >
-        <Section style={{ marginTop: 20 }}>
+        <Section style={{ marginTop: 20, width: '100%', height: '100%' }}>
           <SectionContent>
-            <Text fontWeight="bold" style={{ textAlign: "center" }}>
-              These UI components provided by Rapi UI
-            </Text>
-            <Button
-              style={{ marginTop: 10 }}
-              text="Rapi UI Documentation"
-              status="info"
-              onPress={() => Linking.openURL("https://rapi-ui.kikiding.space/")}
+          <FlatList
+            data={contacts}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}
+            scrollEnabled={true}
             />
-            <Button
+          <Button
               text="Go to second screen"
               onPress={() => {
                 navigation.navigate("SecondScreen");
-              }}
-              style={{
-                marginTop: 10,
-              }}
-            />
-            <Button
-              status="danger"
-              text="Logout"
-              onPress={async () => {
-                const { error } = await supabase.auth.signOut();
-                if (!error) {
-                  alert("Signed out!");
-                }
-                if (error) {
-                  alert(error.message);
-                }
               }}
               style={{
                 marginTop: 10,
@@ -87,3 +122,18 @@ export default function ({
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  contactCard: {
+    borderColor: '#4169e1',
+    borderRadius: 10,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+  },
+  contactName: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
